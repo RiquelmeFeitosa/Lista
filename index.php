dddac9e5e2731ef6308fa07a3e8d8ba5336578c2
Riquelme Farias <riqtube276@gmail.com>
2023-06-13 22:54:31 -0300

    Acre

--- index.php
+++ index.php
@@ -33,7 +33,20 @@
     
     $lista = array(
         'AC' => array(
-            'bandeira' => <img src="" alt="">
+            'bandeira' => '<img width="50" src="https://pt.wikipedia.org/wiki/Ficheiro:Bandeira_do_Acre.svg" alt="AC">',
+            'unidade' => 'Acre',
+            'abreviacao' => 'AC',
+            'sede' => 'Rio Branco',
+            'area' => number_format(164122.2),
+            'populacao' => 795145,
+            'densidade' => 4.30,
+            'pib' => 13622000,
+            'total' => 0.2,
+            'percapita' => 16953.46,
+            'idh' => 0.663,
+            'afabetizacao' => 86.9,
+            'mortkids' => 17,
+            'expectativa' => 73.9 . 'anos'
             )
     )
     ?>
