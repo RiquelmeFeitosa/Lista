cfdd0991944961ef1a865034f056a00712189252
Riquelme Farias <riqtube276@gmail.com>
2023-06-13 22:23:29 -0300

    tabela feita

--- index.php
+++ index.php
@@ -6,6 +6,27 @@
     <title>Lista Sintetica</title>
 </head>
 <body>
-    
+    <h1>Lista Sintetica das Unidades federativas do Brasil</h1>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Bandeiras</th>
+                <th>Unidade Federativa</th>
+                <th>Abreviação</th>
+                <th>Sede de Governo</th>
+                <th>Área(km&#8322;)</th>
+                <th>População(2014)</th>
+                <th>Densidade(2005)</th>
+                <th>PIB(2015)</th>
+                <th>(&#37;Total)(2015)</th>
+                <th>PIB per capita(R&#36;)(2015)</th>
+                <th>IDH(2010)</th>
+                <th>Alfabetização(2016)</th>
+                <th>Mortalidade Infantil(2016)</th>
+                <th>Expectativa de Vida(2016)</th>
+            </tr>
+        </thead>
+    </table>
 </body>
 </html>
